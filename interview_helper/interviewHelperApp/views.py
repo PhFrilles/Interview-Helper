f8b699c729066c5a684b0f2db400b31f8a20adb1
Aadyish <aadyishp@gmail.com>
2026-02-07 17:19:06 +0000

    login working

--- interview_helper/interviewHelperApp/views.py
+++ interview_helper/interviewHelperApp/views.py
@@ -1,3 +1,70 @@
 from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
+from django.db.models import Q, F
+from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.encoding import smart_str
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+import logging
+import re
 # Create your views here.
+
+def start(request):
+    return  render(request, 'welcome.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password) 
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+   
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username already used ')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'password not the same')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+    
+
+def home(request):
+    return render(request, 'home.html')
+
+
+@login_required
+def session_success(request):
+    return redirect("home")
+
+@login_required
+def session_cancel(request):
+    return redirect("home")
