6c8af0e0014744c7e9fb78fbc7ea31275e7b5aa1
Suyogrg <123380700+Suyogrg@users.noreply.github.com>
2026-02-08 04:24:56 +0000

    removed errors

--- interview_helper/interviewHelperApp/views.py
+++ interview_helper/interviewHelperApp/views.py
@@ -144,21 +144,9 @@ def interview(request):
         'question_type': question_type,
         'question_source': question_source
     })
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 def createQuestion(request):
     return render(request, 'createQuestion.html')
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
 #conversion of video to audio
 def convert_video_to_audio(video_path, audio_path=None):
